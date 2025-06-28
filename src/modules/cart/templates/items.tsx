e0cd2448b2b08237fe54caf302617b940983a784
Ajay Kumar Shah <ajaykumarshah998@gmail.com>
2025-06-28 16:35:14 +0530

    Cart page made like farfetch

--- src/modules/cart/templates/items.tsx
+++ src/modules/cart/templates/items.tsx
@@ -1,28 +1,28 @@
-import repeat from "@lib/util/repeat"
-import { HttpTypes } from "@medusajs/types"
-import { Heading, Table } from "@medusajs/ui"
+import repeat from "@lib/util/repeat";
+import { HttpTypes } from "@medusajs/types";
+import { Heading } from "@medusajs/ui";
 
-import Item from "@modules/cart/components/item"
-import { TranslatedTextServer } from "@modules/common/components/translation/translatest-text-server"
-import SkeletonLineItem from "@modules/skeletons/components/skeleton-line-item"
+import Item from "@modules/cart/components/item";
+import { TranslatedTextServer } from "@modules/common/components/translation/translatest-text-server";
+import SkeletonLineItem from "@modules/skeletons/components/skeleton-line-item";
 
 type ItemsTemplateProps = {
-  cart?: HttpTypes.StoreCart
-}
+  cart?: HttpTypes.StoreCart;
+};
 
 const ItemsTemplate = ({ cart }: ItemsTemplateProps) => {
-  const items = cart?.items
+  const items = cart?.items;
   return (
     <div>
-      <div className="mb-8 flex items-center">
-        <Heading className="text-[1.5rem] leading-[2.75rem]">
-          <TranslatedTextServer text=" Shopping Bag" />
+      {/* <div className='mb-8 flex items-center'>
+        <Heading className='text-[1.75rem] leading-[2.75rem]'>
+          <TranslatedTextServer text=' Shopping Bag' />
         </Heading>
-      </div>
+      </div> */}
       {items
         ? items
             .sort((a, b) => {
-              return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
+              return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1;
             })
             .map((item) => {
               return (
@@ -31,16 +31,16 @@ const ItemsTemplate = ({ cart }: ItemsTemplateProps) => {
                   item={item}
                   currencyCode={cart?.currency_code}
                 />
-              )
+              );
             })
         : repeat(5).map((i) => {
-            return <SkeletonLineItem key={i} />
+            return <SkeletonLineItem key={i} />;
           })}
     </div>
-  )
-}
+  );
+};
 
-export default ItemsTemplate
+export default ItemsTemplate;
 
 // import repeat from "@lib/util/repeat"
 // import { HttpTypes } from "@medusajs/types"
--- src/modules/common/components/cart-totals/index.tsx
+++ src/modules/common/components/cart-totals/index.tsx
@@ -1,29 +1,32 @@
-"use client"
+"use client";
 
-import { convertToLocale } from "@lib/util/money"
-import React, { useState, useEffect } from "react"
-import { TranslatedText } from "../translation/translated-text"
-import { HttpTypes, StoreCartShippingMethod } from "@medusajs/types"
-import { setShippingMethod } from "@lib/data/cart"
-import Divider from "@modules/common/components/divider"
+import { convertToLocale } from "@lib/util/money";
+import React, { useState, useEffect } from "react";
+import { TranslatedText } from "../translation/translated-text";
+import { HttpTypes, StoreCartShippingMethod } from "@medusajs/types";
+import { setShippingMethod } from "@lib/data/cart";
+import Divider from "@modules/common/components/divider";
 
 type CartTotalsProps = {
   totals: {
-    id: string
-    total?: number | null
-    subtotal?: number | null
-    tax_total?: number | null
-    shipping_total?: number | null
-    discount_total?: number | null
-    gift_card_total?: number | null
-    currency_code: string
-    shipping_subtotal?: number | null
-    shipping_methods: StoreCartShippingMethod[]
-  },
-  availableShippingMethods: HttpTypes.StoreCartShippingOption[] | null
-}
+    id: string;
+    total?: number | null;
+    subtotal?: number | null;
+    tax_total?: number | null;
+    shipping_total?: number | null;
+    discount_total?: number | null;
+    gift_card_total?: number | null;
+    currency_code: string;
+    shipping_subtotal?: number | null;
+    shipping_methods: StoreCartShippingMethod[];
+  };
+  availableShippingMethods: HttpTypes.StoreCartShippingOption[] | null;
+};
 
-const CartTotals: React.FC<CartTotalsProps> = ({ totals, availableShippingMethods }) => {
+const CartTotals: React.FC<CartTotalsProps> = ({
+  totals,
+  availableShippingMethods,
+}) => {
   const {
     id: cartId,
     currency_code,
@@ -33,93 +36,107 @@ const CartTotals: React.FC<CartTotalsProps> = ({ totals, availableShippingMethod
     discount_total,
     gift_card_total,
     shipping_subtotal: initialShippingSubtotal,
-    shipping_methods
-  } = totals
+    shipping_methods,
+  } = totals;
 
   const [shippingMethodId, setShippingMethodId] = useState<string | null>(
     shipping_methods?.at(-1)?.shipping_option_id || null
-  )
+  );
 
   const handleSetShippingMethod = async (id: string) => {
     // setError(null)
-    let currentId: string | null = null
+    let currentId: string | null = null;
     // setIsLoading(true)
     setShippingMethodId((prev) => {
-      currentId = prev
-      return id
-    })
+      currentId = prev;
+      return id;
+    });
 
     await setShippingMethod({ cartId: cartId, shippingMethodId: id })
       .catch((err) => {
-        setShippingMethodId(currentId)
+        setShippingMethodId(currentId);
         // setError(err.message)
       })
       .finally(() => {
         // setIsLoading(false)
-      })
-  }
+      });
+  };
 
   useEffect(() => {
     if (!shippingMethodId && availableShippingMethods?.length) {
-      const defaultMethod = availableShippingMethods[0]
-      handleSetShippingMethod(defaultMethod.id)
+      const defaultMethod = availableShippingMethods[0];
+      handleSetShippingMethod(defaultMethod.id);
     }
-  }, [availableShippingMethods, shippingMethodId])
+  }, [availableShippingMethods, shippingMethodId]);
 
   const [isLoading, setIsLoading] = useState({
     shipping: true,
     total: true,
-  })
+  });
 
-  const [shipping_subtotal, setShippingSubtotal] = useState<number | null>(null)
-  const [total, setTotal] = useState<number | null>(null)
+  const [shipping_subtotal, setShippingSubtotal] = useState<number | null>(
+    null
+  );
+  const [total, setTotal] = useState<number | null>(null);
 
   const LoadingPlaceholder = () => (
-    <span className="flex items-center justify-center w-6 h-6">
-      <span className="animate-spin rounded-full h-4 w-4 border-2 border-current border-t-transparent text-black"></span>
+    <span className='flex items-center justify-center w-6 h-6'>
+      <span className='animate-spin rounded-full h-4 w-4 border-2 border-current border-t-transparent text-black'></span>
     </span>
-  )
+  );
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShippingSubtotal(initialShippingSubtotal ?? 0)
-      setTotal(initialTotal ?? 0)
+      setShippingSubtotal(initialShippingSubtotal ?? 0);
+      setTotal(initialTotal ?? 0);
       setIsLoading({
         shipping: false,
         total: false,
-      })
-    })
-    return () => clearTimeout(timer)
-  }, [initialShippingSubtotal, initialTotal])
+      });
+    });
+    return () => clearTimeout(timer);
+  }, [initialShippingSubtotal, initialTotal]);
 
   return (
     <div>
-
-      <div className="flex flex-col gap-y-2 txt-medium text-ui-fg-subtle">
-        <div className="flex items-center justify-between">
-          <span className="flex gap-x-1 items-center">
-            <TranslatedText text="Subtotal (excl. shipping costs )" />
+      <div className='flex flex-col gap-y-2 txt-medium'>
+        <div className='flex items-center justify-between'>
+          <span className='flex gap-x-1 items-center text-base'>
+            <TranslatedText text='Subtotal (excl. shipping costs )' />
           </span>
-          <span data-testid="cart-subtotal" data-value={subtotal || 0}>
+          <span
+            data-testid='cart-subtotal'
+            data-value={subtotal || 0}
+            className='text-base'
+          >
             {convertToLocale({ amount: subtotal ?? 0, currency_code })}
           </span>
         </div>
         {!!discount_total && (
-          <div className="flex items-center justify-between">
-            <span><TranslatedText text="Discount" /></span>
+          <div className='flex items-center justify-between'>
+            <span className='text-base'>
+              <TranslatedText text='Discount' />
+            </span>
             <span
-              className="text-ui-fg-interactive"
-              data-testid="cart-discount"
+              className='text-ui-fg-interactive'
+              data-testid='cart-discount'
               data-value={discount_total || 0}
+              className='text-base'
             >
               -{" "}
               {convertToLocale({ amount: discount_total ?? 0, currency_code })}
             </span>
           </div>
         )}
-        <div className="flex items-center justify-between">
-          <span><TranslatedText text="Shipping" /></span>
-          <span data-testid="cart-shipping" data-value={shipping_subtotal || 0}>
+        <div className='flex items-center justify-between'>
+          <span className='text-base'>
+            <TranslatedText text='Shipping' />
+          </span>
+          <span
+            data-testid='cart-shipping'
+            data-value={shipping_subtotal || 0}
+            className='text-base'
+          >
             {isLoading.shipping ? (
               <LoadingPlaceholder />
             ) : (
@@ -127,18 +144,26 @@ const CartTotals: React.FC<CartTotalsProps> = ({ totals, availableShippingMethod
             )}
           </span>
         </div>
-        <div className="flex justify-between">
-          <span className="flex gap-x-1 items-center"><TranslatedText text="Taxes" /></span>
-          <span data-testid="cart-taxes" data-value={tax_total || 0}>
+        <div className='flex justify-between'>
+          <span className='flex gap-x-1 items-center text-base'>
+            <TranslatedText text='Taxes' />
+          </span>
+          <span
+            data-testid='cart-taxes'
+            data-value={tax_total || 0}
+            className='text-base'
+          >
             {convertToLocale({ amount: tax_total ?? 0, currency_code })}
           </span>
         </div>
         {!!gift_card_total && (
-          <div className="flex items-center justify-between">
-            <span><TranslatedText text="Gift card" /></span>
+          <div className='flex items-center justify-between'>
+            <span>
+              <TranslatedText text='Gift card' />
+            </span>
             <span
-              className="text-ui-fg-interactive"
-              data-testid="cart-gift-card-amount"
+              className='text-ui-fg-interactive'
+              data-testid='cart-gift-card-amount'
               data-value={gift_card_total || 0}
             >
               -{" "}
@@ -147,24 +172,32 @@ const CartTotals: React.FC<CartTotalsProps> = ({ totals, availableShippingMethod
           </div>
         )}
       </div>
-      <div className="h-px w-full border-b border-gray-200 mt-4" />
+      <div className='h-px w-full border-b border-gray-200 mt-4' />
       {/* <Divider /> */}
-      <div className="flex items-center justify-between text-ui-fg-base mt-3 mb-3 txt-medium">
-        <span><TranslatedText text="Total" /></span>
-        <span
-          className="txt-xlarge-plus"
-          data-testid="cart-total"
-          data-value={total || 0}
-        >
-          {isLoading.total ? (
-            <LoadingPlaceholder />
-          ) : (
-            convertToLocale({ amount: total ?? 0, currency_code })
-          )}
+      <div className='flex items-center justify-between text-ui-fg-base mt-3 mb-3 txt-medium'>
+        <span className='font-medium text-base'>
+          <TranslatedText text='Total' />
         </span>
+        <div>
+          <span
+            className='txt-xlarge-plus'
+            data-testid='cart-total'
+            data-value={total || 0}
+          >
+            <span className='font-normal text-base uppercase mr-1'>
+              {currency_code}
+            </span>
+            {isLoading.total ? (
+              <LoadingPlaceholder />
+            ) : (
+              convertToLocale({ amount: total ?? 0, currency_code })
+            )}
+          </span>
+          <p className='text-sm'>Import duties included</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartTotals
+export default CartTotals;
